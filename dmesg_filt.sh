8819a793ee76420fb84538d12a5b585925661c6b
TrifonovSV <trifonov32ru@gmail.com>
2022-10-11 14:58:37 +0300

    111022

---
+++ dmesg_filt.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# Просто фильтрик, а может не ожин чтобы не забывать удобные способы мониторинга
+
+sudo dmesg 
+
+# udevadm info
+# This command runs as an query within the database and give that list outs the info as the output.
+
+# udevadm monitor
