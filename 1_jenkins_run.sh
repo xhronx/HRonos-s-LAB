d1d4f74880025e23125858066a09f52ce1f8762f
TrifonovSV <trifonov32ru@gmail.com>
2022-11-08 13:09:14 +0300

    081122

--- 1_jenkins_run.sh
+++ 1_jenkins_run.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 # Запкстить контейнер с дженкинс внутри + первые команды
 
-docker run -d --name jenkins_sf -p 8080:8080 jenkins:2.60.3
+docker run -d --name jenkins_sf -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock jenkins:2.60.3
