b4f8318a7927025dda06eed09b570d83e0def8fc
TrifonovSV <trifonov32ru@gmail.com>
2022-11-22 06:47:54 +0000

    git_aliases.sh 221122+

---
+++ git_aliases.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+# git aliases first commit
+
+git config --global alias.ch checkout
+git config --global alias.br branch
+git config --global alias.co commit
+git config --global alias.st status
+git config --global user.email "trifonov32ru@gmail.com"
+git config --global user.name "TrifonovSV"
