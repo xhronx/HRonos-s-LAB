385ed4db435a38831389369acdf6969f3b0cd17f
TrifonovSV <trifonov32ru@gmail.com>
2022-12-13 06:24:18 +0000

    new mybotalarm.sh 1312

---
+++ scripts/mybotalarm.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+#
+
+# HRonos's Alarm telegram bot send massage
+
+curl -X POST "https://api.telegram.org/bot5809633018:AAE9Q6Cl6M5N9aQZ2Q7vft3UCIjW7y27saY/sendMessage" -d "chat_id=-1001598045534&text=my sample text"
